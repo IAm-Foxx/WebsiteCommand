eff0d1b1d6fb959802c7d03dd9ca12f418c50811
IAm-Kasper <101681960+IAm-Kasper@users.noreply.github.com>
2023-01-16 23:13:10 +0000

    Uploading v1 files

---
+++ WebsiteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+using System.Xml.Serialization;
+
+namespace WebsiteCommand
+{
+    public class WebsiteConfig : IRocketPluginConfiguration
+    {
+        public bool OpenUrlOnJoin;
+        public string JoinUrl;
+        public string JoinDesc;
+        [XmlArrayItem(ElementName = "WebsiteCommand")]
+        public List<WebsiteCommand> WebsiteCommands;
+
+        public void LoadDefaults()
+        {
+            OpenUrlOnJoin = false;
+            JoinUrl = "www.google.com";
+            JoinDesc = "website:";
+            WebsiteCommands = new List<WebsiteCommand> { new WebsiteCommand{CommandName = "default", Desc = "default", Help = "default", Url = "default"}};
+        }
+    }
+
+    public class WebsiteCommand
+    {
+        public string Url;
+        public string Desc;
+        public string CommandName;
+        public string Help;
+    }
+}
